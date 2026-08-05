8c58e54e73e8ed2540c9904c6853d136d9d277db
TalysRodrigues <talesarielr@gmail.com>
2026-08-05 14:27:06 -0300

    Create __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,10 @@
+"""Pacote de regras de negócio da aplicação PurchaseMind AI."""
+
+from services import compras_service, fornecedores_service, notificacoes_service, ia_service
+
+__all__ = [
+    "compras_service",
+    "fornecedores_service",
+    "notificacoes_service",
+    "ia_service",
+]
