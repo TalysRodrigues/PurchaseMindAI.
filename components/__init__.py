ecb50c24e52082a5279d7c1946c9bc2e0e36b796
Tales Ariel Rodrigues <talesarielr@gmail.com>
2026-08-05 20:24:46 -0300

    Remove formatar_data_br from __all__ exports
    Removed 'formatar_data_br' from the exported components.

--- components/__init__.py
+++ components/__init__.py
@@ -1,7 +1,7 @@
 """Pacote de componentes de UI reutilizáveis da aplicação PurchaseMind AI."""
 
 from components.status_badge import render_status_badge, cor_do_status
-from components.compra_card import render_compra_card, formatar_data_br
+from components.compra_card import render_compra_card
 from components.formulario_compra import render_formulario_nova_compra
 from components.notificacoes_widget import render_notificacoes
 
@@ -9,7 +9,6 @@ __all__ = [
     "render_status_badge",
     "cor_do_status",
     "render_compra_card",
-    "formatar_data_br",
     "render_formulario_nova_compra",
     "render_notificacoes",
 ]
